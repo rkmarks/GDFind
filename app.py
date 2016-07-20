819d7e179b4ee1ebbe17a0776ec17ad713f323b1
Ethan Jordan <ejordan@godaddy.com>
2016-07-19 21:57:06 -0700

    Commiting geolocator

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template
+from geopy.geocoders import Nominatim
+
+geolocator = Nominatim()
 
 app = Flask(__name__)
 
--- shared/forms.py
+++ shared/forms.py
@@ -2,5 +2,12 @@ from wtforms import Form, SelectField, StringField, validators
 
 
 class RegistrationForm(Form):
-    keyword = StringField('Keyword Search', [validators.Length(min=1, max=36)])
-    radius = SelectField('Miles Within', choices=[ ][validators.Length(min=6, max=35)])
+    keyword = StringField('Keyword Search',
+                          [
+                            validators.Length(min=1, max=36)
+                            validators.DataRequired()
+                          ])
+    radius = SelectField('Miles Within',
+                         choices=[
+                                                 ],
+                                    [validators.Length(min=6, max=35)])
