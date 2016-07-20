6fc537088f8229e1dbd319b61a3b0d48ea338366
Ethan Jordan <ejordan@godaddy.com>
2016-07-20 00:44:42 -0700

    Test

--- app.py
+++ app.py
@@ -25,7 +25,19 @@ def route_results():
     keyword = request.form['keyword']
     zipcode = request.form['zipcode']
     radius = request.form['radius']
-    return render_template('results.html', radius=radius, keyword=keyword, zipcode=zipcode)
+    location = geolocator.geocode("85254")
+    # Find lat / long
+    data = '{"api_key" : "c9caffc16e51883cfec43b1febf65cb898926037", "fields" : [ "name", "description", "website_url", "open_hours", "categories", "locu_id", "location", "contact" ], "venue_queries" : [ { "categories" : "' + keyword + '", "locu": { "is_publishable": true }, "location" : { "geo" : { "$in_lat_lng_radius" : [' + location.latitude + ', ' + location.longitude +', ' + radius + '] } } } ] }'
+    url = 'https://api.locu.com/v2/venue/search'
+
+    req = urllib2.Request(url, data, {'Content-Type': 'application/json'})
+    f = urllib2.urlopen(req)
+    req = urllib.request.Request(url, data, headers)
+    for x in f:
+        json = x
+    f.close()
+
+    return render_template('results.html', json=json)
 
 
 @app.route('/profile', methods=['GET'])
